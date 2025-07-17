cbdc69395068f6e56777fe1bed7297ca2eac2add
rajankdosanjh <rajan.dosanjh2003@gmail.com>
2025-07-17 12:03:54 +0100

    Theory on NSGA-II

--- app/models.py
+++ app/models.py
@@ -10,26 +10,16 @@ from dataclasses import dataclass
 import datetime
 
 @dataclass
-class User(UserMixin, db.Model):
-    __tablename__ = 'users'
+class Location(db.Model):
+    class Location:
+        def __init__(self, name: str, latitude: float, longitude: float, description: str = "",
+                     tiktok_review_score: float = 0.0):
+            self.name = name
+            self.latitude = latitude
+            self.longitude = longitude
+            self.description = description
 
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
-    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
-    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-    role: so.Mapped[str] = so.mapped_column(sa.String(10), default="Normal")
 
 
-    def __repr__(self):
-        pwh= 'None' if not self.password_hash else f'...{self.password_hash[-5:]}'
-        return f'User(id={self.id}, username={self.username}, email={self.email}, role={self.role}, pwh={pwh})'
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-@login.user_loader
-def load_user(id):
-    return db.session.get(User, int(id))
+class Route(db.Model):
