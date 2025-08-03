eaf0778b26763e20f02de5186b3444324cf17df5
rajankdosanjh <rajan.dosanjh2003@gmail.com>
2025-08-03 12:44:24 +0100

    creating reviews model in models.py

--- app/models.py
+++ app/models.py
@@ -12,8 +12,25 @@ class Location(db.Model):
     latitude: so.Mapped[float] = so.mapped_column(sa.Float)
     longitude: so.Mapped[float] = so.mapped_column(sa.Float)
     category_id: so.Mapped[int] = so.mapped_column(sa.Integer, nullable=False)  # 1=Food, 2=History, etc.
-    rating: so.Mapped[float] = so.mapped_column(sa.Float)  # Aggregated from TikTok data
+    rating: so.Mapped[float] = so.mapped_column(sa.Float)  #
+    reviews:so.Mapped[list["Review"]] = so.relationship(back_populates="location")
+
 
     def __repr__(self):
         return (f'Location(id={self.id}, name={self.name}, latitude={self.latitude}, longitude={self.longitude},'
-                f'category_id={self.category}, rating={self.tiktok_rating})')
+                f'category_id={self.category}, rating={self.tiktok_rating}, reviews={self.reviews})')
+
+
+class Review(db.Model):
+    __tablename__ ='reviews'
+
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    text: so.Mapped[str] = so.mapped_column(sa.String(500))
+    sentiment: so.Mapped[float] = so.mapped_column(sa.Float)
+    location_id: so.Mapped[int]= so.mapped_column(sa.ForeignKey('locations.id'))
+    location: so.Mapped[Location] = so.relationship(back_populates="reviews")
+
+    def __repr__(self):
+        return (f'Review for {self.location.name}: {self.text}')
+
+
